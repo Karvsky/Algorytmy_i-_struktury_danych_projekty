8dae2f87298a6047bb91a415eea350f53b91ebdc
Karol Soppa <karol.soppa50@gmail.com>
2025-03-20 00:02:51 +0100

    change in main

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from generowanie import *
 sys.path.append(os.path.join(os.path.dirname(__file__), 'sortowanie'))
                 
 from sortowanie.shell_sort import *
+from sortowanie.quick_sort import *
+from sortowanie.insertion_sort import *
+from sortowanie.selection_sort import *
 
 m = int(input("Podaj długość zbioru: "))
 
@@ -25,7 +28,24 @@ elif generator == "constant":
 elif generator == "a_shaped":
     s = a_shaped(m)
 
+arr = s
+pom1 = arr
+
 print("Przed sortowaniem: ", s)
+
 print("Po sortowaniu: ", shell_sort(s))
+
+print("Po sortowaniu: ", insertion_sort(s))         #algorytm insertion sort
+
+print("Po sortowaniu: ", selection_sort(s))         #algorytm selection sort
+
+quick_sort(arr, 0, len(arr) - 1, pivot_choice="left")       
+print("Po sortowaniu z pivota lewego:", arr)
+
+arr = pom1
+
+quick_sort(arr, 0, len(arr) - 1, pivot_choice="random")
+print("Po sortowaniu z pivota losowego:", arr)
+
 
 
