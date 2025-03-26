92a6254c336259d1652175d0f245063f4bfb5bf4
ckamil997 <cwiklinski.kamil123@gmail.com>
2025-03-26 01:56:57 +0100

    some changes in main

--- main.py
+++ main.py
@@ -11,50 +11,63 @@ from sortowanie.quick_sort_left_pivot import *
 from sortowanie.quick_sort_random_pivot import *
 os.system('clear')
 
-print("----Program sortujący---- \n-------------------------\n")
-m = int(input("Podaj długość zbioru: "))
+while True:
+    print("----Program sortujący---- \n-------------------------\n")
+    m = input("Podaj długość zbioru: ")
+    try:
+        m = int(m)
+        break 
+    except ValueError:
+        os.system('clear')
+        print("To nie jest poprawna liczba całkowita. Spróbuj ponownie.\n")
+        continue 
+
 os.system('clear')
-generators = ["random_array", "ascending", "descending", "constant", "a_shaped"]
+generators = ["1", "2", "3", "4", "5"]
 
 while True:
     print("----Program sortujący---- \n-------------------------\n")
-    generator = str(input("Wybierz generator (random_array, ascending, descending, constant, a_shaped): "))
+    generator = str(input("Wybierz generator: \nrandom_array - 1\nascending - 2\ndescending - 3\nconstant - 4\na_shaped - 5\n"))
     if generator in generators: break
-    else: print("Podany zły generator, spróbuj ponownie")
+    else: 
+        os.system('clear')
+        print("Podany zły generator, spróbuj ponownie\n")
 
 os.system('clear')
 
 while True:
     print("----Program sortujący---- \n-------------------------\n")
-    sort = str(input("Wybierz algorytm sortujący (shell_sort, insertion_sort, selection_sort, heap_sort, quick_sort_left_pivot, quick_sort_random_pivot): "))
-    if sort in ["shell_sort", "insertion_sort", "selection_sort", "heap_sort", "quick_sort_left_pivot", "quick_sort_random_pivot"]: break
-    else: print("Podany zły algorytm, spróbuj ponownie")
+    sort = str(input("Wybierz algorytm sortujący: \nshell sort - 1\ninsertion sort - 2\nselection sort - 3\nheap sort - 4\nquick sort left pivot - 5\nquick sort random pivot - 6\n"))
+    if sort in ["1", "2", "3", "4", "5", "6"]: break
+    else: 
+        os.system('clear')
+        print("Podany zły algorytm, spróbuj ponownie\n")
 
-if generator == "random_array":
+if generator == "1":
     s = random_array(m)
-elif generator == "ascending":
+elif generator == "2":
     s = ascending(m)
-elif generator == "descending":
+elif generator == "3":
     s = descending(m)
-elif generator == "constant":
+elif generator == "4":
     s = constant(m)
-elif generator == "a_shaped":
+elif generator == "5":
     s = a_shaped(m)
 
 os.system('clear')
-print("----Program sortujący---- \n-------------------------\nPrzed sortowaniem: ", s ,"\n")
+print("----Program sortujący---- \n-------------------------\n \nPrzed sortowaniem: ", s ,"\n")
 
 
-if sort == "shell_sort":
+if sort == "1":
     print("Posortowany zbiór: ", shell_sort(s))
-elif sort == "insertion_sort":
+elif sort == "2":
     print("Posortowany zbiór: ", insertion_sort(s))
-elif sort == "selection_sort":
+elif sort == "3":
     print("Posortowany zbiór: ", selection_sort(s))
-elif sort == "heap_sort":
+elif sort == "4":
     print("Posortowany zbiór: ", heap_sort(s))
-elif sort == "quick_sort_left_pivot":
+elif sort == "5":
     print("Posortowany zbiór: ", quick_sort_left_pivot(s))
-elif sort == "quick_sort_random_pivot":
+elif sort == "6":
     print("Posortowany zbiór: ", quick_sort_random_pivot(s))
 
